a4eeecc73aca80707390fdde95dea35ab660e7ad
Kristen Harrell <kristenharrell@vt.edu>
2021-04-21 10:51:45 -0400

    Added comments and fixed a few commands

--- proc.sh
+++ proc.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+#Submenu script for process operations
 echo "----------------------------------"
 echo "Sub Menu"
 echo "----------------------------------"
+#infinite loop
 while [ true ]
 do
+	#Displays the process options
 	echo "(please enter the number of your selections below)"
 	echo ""
 	echo "1. Show all processes"
@@ -14,20 +17,25 @@ do
 	read SELECT
 	case $SELECT in
 		1)
-			ps -eF
+			#Displays the current processes 
+			ps -ef
 		;;
 		2)
+			#prompts user for pid to kill and kills that process
 			echo "Please enter the PID of the process you would like to kill: "
 			read pid
 			kill $pid
 		;;
 		3)
+			#Displays the top processes
 			top
 		;;	
 		4)
+			#Leaves the submenu and returns to main menu
 			break
 		;;
 		*)
+			#Invalid Option
 			echo "Invalid Option"
 		;;
 	esac
--- proj3.sh
+++ proj3.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+
+#display the date and main menu
 echo $(date)
 echo "----------------------------------"
 echo "Main Menu"
 echo "----------------------------------"
 
+#infinite loop
 while [ true ]
 do
+	#Display Menu Options
 	echo "1. Operating system info"
 	echo "2. Hostname and DNS info"
 	echo "3. Network info"
@@ -18,8 +22,10 @@ do
 	echo "10. Exit"
 	echo -n "Enter your choice [ 1 - 10 ] "
 	read OPT
+	#read in the option chosen
 	case $OPT in
 		1)
+			#System Information: Displays operating system and lsb release
 			echo "----------------------------------"
 			echo "  System Information"
 			echo "----------------------------------"
@@ -27,57 +33,71 @@ do
 			lsb_release -a
 		;;
 		2)
+			#Hostname and DNS Info
 			echo "----------------------------------"
 			echo "  Hostname and DNS info"
 			echo "----------------------------------"
+			#Display hostname, dnsdomain name, and fully qualified domain name
 			echo "Hostname : $(hostname)"
-			echo "DNS domain : $(hostname -d)"
+			echo "DNS domain : $(dnsdomainname)"
 			echo "Fully qualified domain name: $(hostname -f)"
-			echo "Network address (IP) : $(hostname -I)"
+			#Network address and dns name server
+			echo "Network address (IP) : $(hostname -i)"
 			echo "DNS name servers (DNS IP) : $(hostname -id)"
 		;;
 		3)
+			#Network information
 			echo "----------------------------------"
 			echo "  Network information"
 			echo "----------------------------------"
+			#Display total network interfaces
 			echo "Total network interfaces found: $(ls -A /sys/class/net | wc -l)"
+			#Display TP Address Info
 			echo "*** TP Addresses Information ***"
-			ip a
+			ip addr show
+			#Display network routing table
 			echo "***********************"
 			echo "*** Network routing ***"
 			echo "***********************"
-			netstat -nr
+			netstat -rnf
+			#Display interface trafic information 
 			echo "*************************************"
 			echo "*** Interface traffic information ***"
 			echo "*************************************"
 			netstat -i
 		;;	
 		4)
+			#Who is online
 			echo "----------------------------------"
 			echo "  Who is online"
 			echo "----------------------------------"
+			#displays who is online in a good format
 			who | awk '{print $1","$2","$3" "$4","$5}' | column -s"," -t -N NAME,LINE,TIME,COMMENT
 
 		;;
 		5) 
+			#Display list of last logged in users
 			echo "----------------------------------"
 			echo "  List of last logged in users"
 			echo "----------------------------------"
 			last
 		;;
 		6) 
+			#Display public IP information
 			echo "----------------------------------"
 			echo "  Public IP Information"
 			echo "----------------------------------"
 			dig +short myip.opendns.com @resolver1.opendns.com
 		;;
 		7) 
+			#Display Disk Usage Info
 			echo "----------------------------------"
 			echo "  Disk Usage Info"
 			echo "----------------------------------"
-			df --output=pcent,source | sed 1d | sort -n
+			df --output=pcent,target | sed 1d | sort -n
 		;;
 		8) 
+			#Generate filetree.html, a file tree of the home directory to the current directory
 			echo "----------------------------------"
 			echo "  My home file-tree"
 			echo "----------------------------------"
@@ -86,21 +106,26 @@ do
 			echo "File tree successfully saved to filetree.html"
 		;;
 		9) 
+			#Runs the submenu for process operations
 			sh ./proc.sh
 		;;
 		10) 
+			#Exits the program
 			break
 		;;
 		*)
+			#Invalid option
 			echo "Invalid Option"
 		;;
 	esac
 	
+	#If a process operation, display main menu
 	if [ $OPT -eq 9 ]  
 	then 
 		echo "----------------------------------"
 		echo "Main Menu"
 		echo "----------------------------------"
+	#if not quit, then require a return press to continue
 	elif [ $OPT -ne 10 ] 
 	then
 		read -r -s -p $'Press [Enter] key to continue...'
